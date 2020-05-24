ad6be6b45cfa93efb8e88eb146bf81e71297bd1e
AhmedHalat <ahmedhalat@gmail.com>
2020-05-24 14:45:47 -0400

    Created a home file, changed posts, added ServerManager for fun

--- content/themes/blog-overflow/assets/js/index.js
+++ content/themes/blog-overflow/assets/js/index.js
@@ -4,11 +4,19 @@
  */
 
 window.onload = function (){
+
   var burger = document.querySelector('.burger');
   var nav = document.querySelector('#'+ burger.dataset.target)
 
   burger.addEventListener('click', function () {
     burger.classList.toggle('is-active');
     nav.classList.toggle('is-active')
-  })
+  });
+  const getAge = birthDate => Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e+10);
+
+  let age = getAge("2001-06-16")
+  for (let el of document.getElementsByClassName("age")) {
+    el.innerHTML = age;
+  }
+
 }
